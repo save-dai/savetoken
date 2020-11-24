8f043245c057a098c6bb53f5e0b4eaaf4426f6b3
Doug Crescenzi <doug@upstateinteractive.io>
2020-11-23 19:59:49 -0500

    removed constructor from CompoundAdapter; cleaned up tests

--- contracts/interfaces/IInsurance.sol
+++ contracts/interfaces/IInsurance.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.6.0;
 
 interface IInsurance {
     function buyInsurance(uint256 amount) external returns (uint256);
-
-    // function getCostOfInsuranceToken(uint256 amount) external returns (uint256);
+	function getCostOfInsuranceToken(uint256 amount) external returns (uint256);
     // function exerciseInsurance(uint256 amount) external returns (uint256);
 }
