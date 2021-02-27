466ff1494d17657287b24368820183165c41fb5e
Doug Crescenzi <doug@upstateinteractive.io>
2021-02-27 13:18:39 -0500

    Implemented hold and getCostOfAsset in new AaveAdapter

---
+++ contracts/interfaces/IAToken.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.8.0;
+
+// Aave aToken interface
+interface IAToken {
+  function mint(
+    address user,
+    uint256 amount,
+    uint256 index
+  ) external returns (bool);
+  function burn(
+    address user,
+    address receiverOfUnderlying,
+    uint256 amount,
+    uint256 index
+  ) external;
+  function mintToTreasury(uint256 amount, uint256 index) external;
+  function transferOnLiquidation(
+    address from,
+    address to,
+    uint256 value
+  ) external;
+
+  function transferUnderlyingTo(address user, uint256 amount) external returns (uint256);
+  function balanceOf(address account) external view returns (uint256);
+}
